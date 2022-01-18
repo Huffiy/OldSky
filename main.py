35e82384e938772544a0548e77bcaa03f1767940
Huffiy <davide.ballerini@polomanettiporciatti.edu.it>
2022-01-18 08:56:14 +0100

    Removed HTTP webserver

--- main.py
+++ main.py
@@ -1,47 +1,23 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
-
-# WebServer Settings
-hostName = "localhost"
-serverPort = 8080
+import requests
 
 if __name__ == "__main__":
-    print("WebServer started http://%s:%s" % (hostName, serverPort))
     # Auth Spotify API
     client_credentials_manager = SpotifyClientCredentials(client_id='f6046cde45a54ae895dfea1dcf09aeb5',
                                                           client_secret='33e06b00af1a4cd69b7b9b0528ce4737')
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-    lz_uri = 'spotify:artist:59rqdbDiB9oXuZggah1syh' # Artist URI
+    lz_uri = 'spotify:artist:6NJpuZla7aHLZEfMBSMcH8' # Artist URI
     spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     results = spotify.artist_top_tracks(lz_uri)
     for track in results['tracks'][:1]:
-        trackName = track['name']
-        trackMP3 = track['preview_url']
-        trackAlbum = track['album']['images'][0]['url']
-        print(trackName + trackAlbum + trackMP3)
+        urltrackName = track['name']
+        urltrackMP3 = track['preview_url']
+        urltrackAlbum = track['album']['images'][0]['url']
 
-    # WebServer Content
-    class MyServer(BaseHTTPRequestHandler):
-        def do_GET(self):
-            # WebServer Content
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(bytes("<img src=" + trackAlbum + "> <br>", "utf-8"))
-            self.wfile.write(bytes("<audio controls> <source src=" + trackMP3 + " type=\"audio/mpeg\">", "utf-8"))
-
-
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-
-
-    webServer.server_close()
-    print("WebServer stopped.")
+        # Save files to /downloads directory
+        getTrackImg = requests.get(urltrackAlbum)
+        getTrackMP3 = requests.get(urltrackMP3)
+        open('downloads/img/r.png', 'wb').write(getTrackImg.content)
+        open('downloads/mp3/r.mp3', 'wb').write(getTrackMP3.content)
