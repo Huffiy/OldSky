6175bd80d658d2e3f8d40071859a5e0aa3b64489
Huffiy <davide.ballerini@polomanettiporciatti.edu.it>
2022-04-26 13:22:58 +0200

    First HTML/CSS commit

--- frontend/app.php
+++ frontend/app.php
@@ -11,10 +11,20 @@ else {
 <!DOCTYPE html>
 <head>
     <title>OldSky | Home</title>
+    <link rel="stylesheet" href="./css/app.css">
 </head>
 <body>
+    <div class="navbar">
+    <form action="./playlists">
+    <input type="submit" value="Le mie playlist" />
+    </form>
+    <form action="./profile">
+    <input type="submit" value="Profilo" />
+    </form>
+    </div>
     <h1>OldSky</h1>
     <h2>Welcome <?php echo $_SESSION['username']; ?></h2>
     <a href="./logout.php">Logout</a>
     <a href="./app.php">Go to app</a>
+
 </body>
